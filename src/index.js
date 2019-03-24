983ece07d15d333ab03098b36339361691a7051d
alexanderrabetsky <3820168Rabetsky>
2019-03-24 22:44:52 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+    const result = {};
+    const moneyEquals = {
+        50: 'H',
+        25: 'Q',
+        10: 'D',
+        5: 'N',
+        1: 'P'
+    };
+    const usedMoney   = [50, 25, 10, 5, 1];
+
+    function writeInObj(elem, obj, increase) {
+        obj[moneyEquals[elem]] = increase;
+    }
+
+    if (currency > 10000) {
+        result.error = "You are rich, my friend! We don't have so much coins for exchange";
+    } else if (currency > 0) {
+        usedMoney.forEach((element) => {
+            if (currency / element >= 1) {
+                writeInObj(element, result, Math.floor(currency / element));
+                currency = currency % element;
+            }
+            else if (currency / element < 1
+                    && currency / element >= 0)
+            {
+                return
+            }
+        });
+    }
+    return result;
+};
